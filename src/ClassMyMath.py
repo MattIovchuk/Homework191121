b42b10784c7633e24293e173028cc59a5995490a
MattIovchuk <90251848+MattIovchuk@users.noreply.github.com>
2021-11-28 12:49:37 +0300

    Add files via upload

---
+++ src/ClassMyMath.py
@@ -0,0 +1,50 @@
+class MyMath:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def addition(self):
+        return print(self.a + self.b)
+
+    def multiplication(self):
+        return print(self.a * self.b)
+
+    def division(self):
+        try:
+            self.a / self.b
+        except ZeroDivisionError:
+            print("MyMath error : You can't divide by zero.")
+        else:
+            return print(self.a / self.b)
+
+    def subtraction(self):
+        return print(self.a - self.b)
+
+    def pow(self):
+        return print(self.a ** self.b)
+
+    def sqrt(self):
+        if self.a < 0:
+            print("MyMath error : You cannot take the root of a negative number.")
+        else:
+            return print(self.a ** 0.5)
+
+
+ans = MyMath(4, 4)
+ans.addition()
+ans.subtraction()
+ans.multiplication()
+ans.division()
+ans.pow()
+ans.sqrt()
+# 1) +; 2) *; 3) /; 4) -; 5) pow; 6) sqrt; a, b = 4, 4
+
+
+print()
+
+
+err = MyMath(-1, 0)
+err.division()
+err.sqrt()
+# 1) /; 2) sqrt; a, b = -1 0
+input("Press Enter to exit...")
