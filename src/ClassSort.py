995658d4b32577f4bd11885a7b72dfd39df79965
MattIovchuk <90251848+MattIovchuk@users.noreply.github.com>
2021-11-28 13:02:35 +0300

    Add files via upload

---
+++ src/ClassSort.py
@@ -0,0 +1,14 @@
+class Sort:
+    def __init__(self, a):
+        self.a = a
+
+    def bubble(self):
+        for i in range(len(self.a) - 1):
+            for j in range(len(self.a) - i - 1):
+                if self.a[j] > self.a[j + 1]:
+                    self.a[j], self.a[j + 1] = self.a[j + 1], self.a[j]
+        return print(self.a)
+
+
+my_list = Sort([5, 3, 4, 1, 2])
+my_list.bubble()
